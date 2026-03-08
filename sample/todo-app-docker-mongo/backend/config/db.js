568a9da49acf7972ea7427e97b599561d1460dfd
Nguyen Duc <104844845+DukeCUD@users.noreply.github.com>
2026-03-08 21:55:38 +0700

    Fix error dependent from backend

--- sample/todo-app-docker-mongo/backend/config/db.js
+++ sample/todo-app-docker-mongo/backend/config/db.js
@@ -1,17 +1,26 @@
 // config/db.js
 // Kết nối MongoDB với Mongoose
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
+let retries = 5;
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
-
-    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
-    console.log(`📊 Database: ${conn.connection.name}`);
-
-  } catch (error) {
-    console.error(`❌ MongoDB Connection Error: ${error.message}`);
-    process.exit(1);
+  while (retries) {
+    try {
+      const conn = await mongoose.connect(process.env.MONGODB_URI);
+      console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+      console.log(`📊 Database: ${conn.connection.name}`);
+      break;
+    } catch (error) {
+      console.error(
+        `❌ MongoDB Connection Error (${retries} retries left): ${error.message}`,
+      );
+      retries -= 1;
+      if (retries === 0) {
+        process.exit(1);
+      }
+      // Wait 3s before retrying
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+    }
   }
 };
 
